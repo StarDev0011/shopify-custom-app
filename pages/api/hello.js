968ecd174d12c25652708a0866f8b4c1bc2cd48d
StarDev0011 <ondre.williams.dev@gmail.com>
2022-03-16 09:59:54 -0700

    api connection

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,12 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  const domain = process.env.SHOPIFY_STORE_DOMAIN
-  const adminAccessToken = process.env.SHOPIFY_ADMIN_ACCESSTOKEN
+  async function ShopifyData(query) {
+    const URL = `https://cac-prototype.myshopify.com/admin/api/2022-01/graphql.json`
+    const options = {
+      endpoint: URL,
+      method: "POST",
+      headers: {
+        "X-Shopify-Access-Token": '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',
+        "Accept": "application/json",
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ query })
+    }
+  
+    try {
+      const data = await fetch(URL, options).then(response => {
+        return response.json()
+      })
+  
+      return data
+    } catch (error) {
+        console.log("err",error)
+    }
+  }
 
-  const URL = `https://cac-prototype.myshopify.com/admin/api/2022-01/graphql.json`
-
-  const query = `
+  async function getAllCustomers() {
+    const query = `
     {
     customers(first: 5) {
       edges {
@@ -18,22 +38,13 @@ export default function handler(req, res) {
     }
   }
   `
-
-  const options = {
-    endpoint: URL,
-    method: "POST",
-    headers: {
-      "X-Shopify-Access-Token": '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',
-      "Accept": "application/json",
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ query })
+  
+    const response = await ShopifyData(query)
+    const allcustomers = response.data.customers.edges ? response.data.customers.edges : []
+    console.log("All",allcustomers)
+    res.status(200).json({allcustomers})
+    return allcustomers
   }
-
-    fetch(URL, options).then(response => {
-      // return response.json()
-      console.log(response)
-      res.status(200).send(response)
-    })
-    
+  const users = getAllCustomers()
+  console.log("users", users)
 }
