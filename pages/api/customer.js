d18079a7a5bcf3b2478885fea3f17d3f611a9b50
StarDev0011 <ondre.williams.dev@gmail.com>
2022-03-16 10:50:46 -0700

    get the current customer data

--- pages/api/customer.js
+++ pages/api/customer.js
@@ -1,8 +1,50 @@
 export default function handler(req, res) {
     if (req.method === 'POST') {
-    res.status(200).json({ id: req.body.id })
+        async function ShopifyData(query) {
+            const URL = `https://cac-prototype.myshopify.com/admin/api/2022-01/graphql.json`
+            const options = {
+                endpoint: URL,
+                method: "POST",
+                headers: {
+                    "X-Shopify-Access-Token": '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',
+                    "Accept": "application/json",
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({ query })
+            }
+
+            try {
+                const data = await fetch(URL, options).then(response => {
+                    return response.json()
+                })
+
+                return data
+            } catch (error) {
+                console.log("err", error)
+            }
+        }
+        async function getCustomerData() {
+            const query = `
+            {
+            customer(id: "gid://shopify/Customer/1753015910478") {
+              id
+              tags
+              displayName
+              ordersCount
+            }
+          }
+          `
+
+            const response = await ShopifyData(query)
+            const allcustomers = response.data.customer ? response.data.customer : []
+            console.log("All", allcustomers)
+            res.status(200).json({ allcustomers })
+            return allcustomers
+        }
+        const user = getCustomerData()
+        console.log("users", user)
     }
     else {
         res.status(200).send("Get")
     }
-  }
+}
